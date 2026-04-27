b008275aff8bc6720c471f30ea0f247ceb81215f
Fred Emmott <git@fredemmott.com>
2026-04-27 12:52:48 -0500

    ✨ Add `felly::unique_unlock`
    fixes #12

---
+++ include/felly/unique_unlock.hpp
@@ -0,0 +1,44 @@
+// Copyright 2026 Fred Emmott <fred@fredemmott.com>
+// SPDX-License-Identifier: MIT
+#pragma once
+
+#include <utility>
+
+namespace felly::inline unique_unlock_types {
+
+template <class T>
+struct unique_unlock {
+  unique_unlock() = delete;
+  explicit unique_unlock(T& lock) : mLock(&lock) { lock.unlock(); }
+
+  ~unique_unlock() {
+    if (mLock) {
+      mLock->lock();
+    }
+  }
+
+  unique_unlock(unique_unlock&& other) noexcept {
+    mLock = std::exchange(other.mLock, nullptr);
+  }
+
+  unique_unlock& operator=(unique_unlock&& other) noexcept {
+    if (mLock) {
+      mLock->lock();
+    }
+    mLock = std::exchange(other.mLock, nullptr);
+    return *this;
+  }
+
+  unique_unlock(const unique_unlock&) = delete;
+  unique_unlock& operator=(const unique_unlock&) = delete;
+
+  [[nodiscard]]
+  bool owns_lock() const noexcept {
+    return mLock != nullptr;
+  }
+
+ private:
+  T* mLock {nullptr};
+};
+
+}// namespace felly::inline unique_unlock_types
---
+++ tests/unique_unlock.cpp
@@ -0,0 +1,54 @@
+// Copyright 2026 Fred Emmott <fred@fredemmott.com>
+// SPDX-License-Identifier: BSL-1.0
+
+#include <catch2/catch_test_macros.hpp>
+#include <felly/unique_unlock.hpp>
+
+#include <mutex>
+
+TEST_CASE("unique_unlock") {
+  std::mutex m;
+  std::unique_lock lock {m};
+  CHECK(lock.owns_lock());
+  {
+    const felly::unique_unlock unlock {lock};
+    CHECK(unlock.owns_lock());
+    CHECK_FALSE(lock.owns_lock());
+  }
+  CHECK(lock.owns_lock());
+
+  SECTION("move-construct") {
+    felly::unique_unlock a {lock};
+    CHECK(a.owns_lock());
+    felly::unique_unlock b {std::move(a)};
+    CHECK(b.owns_lock());
+    CHECK_FALSE(a.owns_lock());
+    CHECK_FALSE(lock.owns_lock());
+  }
+  CHECK(lock.owns_lock());
+
+  SECTION("move-assign") {
+    felly::unique_unlock a {lock};
+    felly::unique_unlock b {std::move(a)};
+    a = std::move(b);
+    CHECK(a.owns_lock());
+    CHECK_FALSE(b.owns_lock());
+    CHECK_FALSE(lock.owns_lock());
+  }
+
+  CHECK(lock.owns_lock());
+
+  SECTION("move-assign over valid lock") {
+    felly::unique_unlock a {lock};
+    std::mutex mutexB;
+    std::unique_lock lockB {mutexB};
+    felly::unique_unlock b {lockB};
+    a = std::move(b);
+    CHECK(lock.owns_lock());
+    CHECK_FALSE(lockB.owns_lock());
+    CHECK(a.owns_lock());
+    CHECK_FALSE(b.owns_lock());
+  }
+
+  CHECK(lock.owns_lock());
+}
