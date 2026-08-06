a1635926f28ff90a09d846edf6496f8d154ece64
Fred Emmott <git@fredemmott.com>
2026-08-06 15:17:07 -0500

    clang-format

--- tests/numeric_cast.cpp
+++ tests/numeric_cast.cpp
@@ -25,14 +25,14 @@ TEST_CASE("numeric_cast: Integral to Integral", "[numeric_cast][integral]") {
   }
 
   SECTION("Out of range: Positive overflow") {
-    constexpr auto TooHigh
-      = static_cast<long long>(std::numeric_limits<int>::max()) + 1;
+    constexpr auto TooHigh =
+      static_cast<long long>(std::numeric_limits<int>::max()) + 1;
     CHECK_THROWS_AS(numeric_cast<int>(TooHigh), numeric_cast_range_error);
   }
 
   SECTION("Out of range: Negative overflow") {
-    constexpr auto TooSmall
-      = static_cast<long long>(std::numeric_limits<int>::lowest()) - 1;
+    constexpr auto TooSmall =
+      static_cast<long long>(std::numeric_limits<int>::lowest()) - 1;
     CHECK_THROWS_AS(numeric_cast<int>(TooSmall), numeric_cast_range_error);
   }
 
@@ -45,9 +45,7 @@ TEST_CASE("numeric_cast: Integral to Integral", "[numeric_cast][integral]") {
 TEST_CASE(
   "numeric_cast: Floating Point to Floating Point",
   "[numeric_cast][float]") {
-  SECTION("Standard conversion") {
-    CHECK(numeric_cast<float>(1.5) == 1.5f);
-  }
+  SECTION("Standard conversion") { CHECK(numeric_cast<float>(1.5) == 1.5f); }
 
   SECTION("NaN handling") {
     CHECK(std::isnan(numeric_cast<float>(NAN)));
@@ -62,8 +60,8 @@ TEST_CASE(
   }
 
   SECTION("Past limits") {
-    constexpr auto EpsilonF
-      = static_cast<double>(std::numeric_limits<float>::epsilon());
+    constexpr auto EpsilonF =
+      static_cast<double>(std::numeric_limits<float>::epsilon());
     constexpr auto LowestF = std::numeric_limits<float>::lowest();
     constexpr auto MaxF = std::numeric_limits<float>::max();
     constexpr auto TooLow = static_cast<double>(LowestF) + (LowestF * EpsilonF);
@@ -77,7 +75,9 @@ TEST_CASE(
   "numeric_cast: Float to Integral",
   "[numeric_cast][float_to_integral]") {
   SECTION("nan") {
-    CHECK_THROWS_AS(numeric_cast<int>(std::numeric_limits<float>::quiet_NaN()), numeric_cast_range_error);
+    CHECK_THROWS_AS(
+      numeric_cast<int>(std::numeric_limits<float>::quiet_NaN()),
+      numeric_cast_range_error);
   }
   SECTION("unsigned narrowing at limits") {
     constexpr auto Lowest = std::numeric_limits<uint32_t>::lowest();
@@ -104,14 +104,14 @@ TEST_CASE(
   }
 
   SECTION("Out of range: Positive overflow") {
-    constexpr auto TooHigh
-      = static_cast<double>(std::numeric_limits<int>::max()) + 1;
+    constexpr auto TooHigh =
+      static_cast<double>(std::numeric_limits<int>::max()) + 1;
     CHECK_THROWS_AS(numeric_cast<int>(TooHigh), numeric_cast_range_error);
   }
 
   SECTION("Out of range: Negative overflow") {
-    constexpr auto TooLow
-      = static_cast<double>(std::numeric_limits<int>::lowest()) - 1;
+    constexpr auto TooLow =
+      static_cast<double>(std::numeric_limits<int>::lowest()) - 1;
     CHECK_THROWS_AS(numeric_cast<int>(TooLow), numeric_cast_range_error);
   }
 
@@ -127,8 +127,7 @@ TEST_CASE(
 TEST_CASE(
   "numeric_cast: Integral to Float",
   "[numeric_cast][integral_to_float]") {
-
-  constexpr auto High  = static_cast<uint32_t>(1) << 31;
+  constexpr auto High = static_cast<uint32_t>(1) << 31;
   CHECK(numeric_cast<float>(High) == High);
 
   constexpr auto Low = std::numeric_limits<int32_t>::lowest();
